b21aa3bd5179d4c0e63bb90ed74f306e6836ada3
Joe Huss <detain@interserver.net>
2016-09-22 13:11:22 +0000

    adding some informatoin about it.. was typed up to mike and realized it would work good for an about blurb in the comment header...
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@59 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/class.crud.php
+++ include/class.crud.php
@@ -1,6 +1,26 @@
 <?php
 	/**
 	 * CRUD Class
+	 *
+	 * its got a very customizable output and customizable field handling ,  but aiming for it to automatically
+	 * generate an optiomal page w/out the need for customizing in most cases
+	 *
+	 * i figure most all pages in my are based around a single query/table , each page just having basically a
+	 * different query, but essentially all doing the same basic things with it, building a list of records, a
+	 * form to add or edit them, or simply display a record, or a page might be a group of several of these
+	 * things...
+	 *
+	 * so my goal w this is to automatically generate as much as possible for each page using only the given
+	 * query, and where customized handling for something is needed make that process as simple as possible as
+	 * well, so pages can be reworked and code reduced to just the fewest bits of information unique to that
+	 * page and get improved layouts+validation/form-handling as a side effect
+	 *
+	 * CRUD classes themselves are very popular and used in most all big frameworks, but from extensive
+	 * research (over the past few years of trying to decide which one to use) they all seem rather hard
+	 * to implement or at least a lot of code just to setup a page using it , my approach is i think the
+	 * ideal way to do it taking the best of what ive found in other CRUD classes but requiring less code
+	 * to set it up.
+	 *
 	 * Last Changed: $LastChangedDate$
 	 * @author detain
 	 * @version $Revision$
