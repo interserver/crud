6e0e07ea29b1d79affbd8d2d2e9fd8592f434d65
Joe Huss <detain@interserver.net>
2016-10-07 08:46:13 +0000

    updates to my scrit tha genreates basic crud init lines and files for each of the jqgrid setups
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@140 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- scripts/crud/make_cruds.php
+++ scripts/crud/make_cruds.php
@@ -1,33 +1,34 @@
 #!/usr/bin/php
 <?php
-require_once(__DIR__ . '/include/functions.inc.php');
+require_once(__DIR__ . '/../../include/functions.inc.php');
+add_js('bootstrap');
 require_once(INCLUDE_ROOT . '/rendering/class.crud.php');
 $cmd = "ls " . INCLUDE_ROOT . "/forms/*json;";
 $files = explode("\n", trim(`$cmd`));
 $function_req = '';
-$function_req .= "			'cruds' => '/admin/cruds.php',\n";
+$function_req .= "			'cruds' => '/crud/cruds.php',\n";
 $li = array();
+$li[] = "<a href='#' class='list-group-item active'>CRUD Page Links</a>";
 foreach ($files as $filepath) {
 	$func = basename($filepath, '.json');
 	$data = json_decode(file_get_contents($filepath), true);
 	$title = $data['caption'];
 	$query = "select {$data['queryfields']} {$data['query']}";
-	$function_req .= "			'crud_{$func}' => '/admin/crud_{$func}.php',\n";
-	$li[] = "<li><a href='?choice=none.crud_{$func}'>{$func} - {$title}</li>";
+	$function_req .= "			'crud_{$func}' => '/crud/crud_{$func}.php',\n";
+	$li[] = "<a href='?choice=none.crud_{$func}' class='list-group-item'>
+<span class='label label-info'>{$func}</span> - {$title}</a>";
 	file_put_contents(INCLUDE_ROOT . '/crud/crud_' . $func . '.php', '<'.'?'."php
 function crud_{$func}() {
 	require_once(INCLUDE_ROOT . '/rendering/class.crud.php');
-	\$crud = crud::init(\"{$query}\")
+	\$crud = crud::init(\"{$query}\"" . (in_array($data['module'], array('innertell', 'mb', 'powerdns')) ? ", '{$data['module']}'" : '') . ")
 		->set_title(\"{$title}\")
 		->go();
 }
 ");
 file_put_contents(INCLUDE_ROOT . '/crud/cruds.php', '<'.'?'."php
 function cruds() {
-	add_output('<ul>
-".implode("\n",$li)."
-</ul>');	
-}
-");
+	add_js('bootstrap');
+	add_output(\"<div class='list-group' style='width: 500px; text-align: left;'>\n" . implode("\n",$li)."\n</div>\n\");
+}");
 file_put_contents(INCLUDE_ROOT . '/function_requirements.new', $function_req);
 }
