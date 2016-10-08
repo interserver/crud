165d836b49606e34c58180d78e1c74dcd96405c7
Joe Huss <detain@interserver.net>
2016-10-08 19:38:56 +0000

    updates to the cruds list - moved from a vertcal list to a table with cells - rounded the corners - added a status colored label for the links to differentiate their progress - added a key so ya konw what each color means - moved the list off into its own recallable function - added the 2 cruds i was previously workig on to the list
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@142 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/crud/cruds.php
+++ include/crud/cruds.php
@@ -1,130 +1,120 @@
 <?php
+
 function cruds() {
 	add_js('bootstrap');
-	add_output("<div class='list-group' style='width: 500px; text-align: left;'>
-<a href='#' class='list-group-item active'>CRUD Page Links</a>
-<a href='?choice=none.crud_abuse' class='list-group-item'>
-<span class='label label-info'>abuse</span> - Abuse</a>
-<a href='?choice=none.crud_active_packages' class='list-group-item'>
-<span class='label label-info'>active_packages</span> - Active Packages</a>
-<a href='?choice=none.crud_admin_tickets' class='list-group-item'>
-<span class='label label-info'>admin_tickets</span> - Admin Tickets</a>
-<a href='?choice=none.crud_admin_tickets_widget' class='list-group-item'>
-<span class='label label-info'>admin_tickets_widget</span> - Admin Tickets</a>
-<a href='?choice=none.crud_backups' class='list-group-item'>
-<span class='label label-info'>backups</span> - Backups</a>
-<a href='?choice=none.crud_backups_list' class='list-group-item'>
-<span class='label label-info'>backups_list</span> - Backup List</a>
-<a href='?choice=none.crud_coupons' class='list-group-item'>
-<span class='label label-info'>coupons</span> - Coupons</a>
-<a href='?choice=none.crud_customers' class='list-group-item'>
-<span class='label label-info'>customers</span> - Customers</a>
-<a href='?choice=none.crud_dedicated_list' class='list-group-item'>
-<span class='label label-info'>dedicated_list</span> - Dedicated List</a>
-<a href='?choice=none.crud_dns_manager' class='list-group-item'>
-<span class='label label-info'>dns_manager</span> - DNS Manager</a>
-<a href='?choice=none.crud_domains' class='list-group-item'>
-<span class='label label-info'>domains</span> - Domains</a>
-<a href='?choice=none.crud_domains_list' class='list-group-item'>
-<span class='label label-info'>domains_list</span> - Domains</a>
-<a href='?choice=none.crud_fantastico_list' class='list-group-item'>
-<span class='label label-info'>fantastico_list</span> - Fantastico License List</a>
-<a href='?choice=none.crud_form_manager' class='list-group-item'>
-<span class='label label-info'>form_manager</span> - Form Manager</a>
-<a href='?choice=none.crud_forum_rss' class='list-group-item'>
-<span class='label label-info'>forum_rss</span> - Latest Forum Posts</a>
-<a href='?choice=none.crud_history_log' class='list-group-item'>
-<span class='label label-info'>history_log</span> - History Log</a>
-<a href='?choice=none.crud_innertell_orders' class='list-group-item'>
-<span class='label label-info'>innertell_orders</span> - Dedicated Server Orders</a>
-<a href='?choice=none.crud_innertell_pending_orders' class='list-group-item'>
-<span class='label label-info'>innertell_pending_orders</span> - Pending Dedicated Server Orders</a>
-<a href='?choice=none.crud_innertell_pending_orders_new' class='list-group-item'>
-<span class='label label-info'>innertell_pending_orders_new</span> - Pending Server Orders</a>
-<a href='?choice=none.crud_innertell_search' class='list-group-item'>
-<span class='label label-info'>innertell_search</span> - Search Results</a>
-<a href='?choice=none.crud_invoices' class='list-group-item'>
-<span class='label label-info'>invoices</span> - Invoices</a>
-<a href='?choice=none.crud_last_logins' class='list-group-item'>
-<span class='label label-info'>last_logins</span> - Last Logn Locations</a>
-<a href='?choice=none.crud_licenses' class='list-group-item'>
-<span class='label label-info'>licenses</span> - Licenses</a>
-<a href='?choice=none.crud_licenses_list' class='list-group-item'>
-<span class='label label-info'>licenses_list</span> - License List</a>
-<a href='?choice=none.crud_modernbill_client_list' class='list-group-item'>
-<span class='label label-info'>modernbill_client_list</span> - Modernbill Client List</a>
-<a href='?choice=none.crud_modernbill_invoice_list' class='list-group-item'>
-<span class='label label-info'>modernbill_invoice_list</span> - Modernbill Invoice List</a>
-<a href='?choice=none.crud_modernbill_package_list' class='list-group-item'>
-<span class='label label-info'>modernbill_package_list</span> - Modernbill Package Listing</a>
-<a href='?choice=none.crud_monitoring_list' class='list-group-item'>
-<span class='label label-info'>monitoring_list</span> - Monitored Systems</a>
-<a href='?choice=none.crud_month_payment_totals' class='list-group-item'>
-<span class='label label-info'>month_payment_totals</span> - Payments This Month</a>
-<a href='?choice=none.crud_new_vps' class='list-group-item'>
-<span class='label label-info'>new_vps</span> - Newest VPS Signups</a>
-<a href='?choice=none.crud_packages' class='list-group-item'>
-<span class='label label-info'>packages</span> - Your Packages</a>
-<a href='?choice=none.crud_paypal_history' class='list-group-item'>
-<span class='label label-info'>paypal_history</span> - Paypal History</a>
-<a href='?choice=none.crud_paypal_transactions' class='list-group-item'>
-<span class='label label-info'>paypal_transactions</span> - Paypal Transactions</a>
-<a href='?choice=none.crud_pending_vps_list' class='list-group-item'>
-<span class='label label-info'>pending_vps_list</span> - Pending Virtual Private Servers (VPS)</a>
-<a href='?choice=none.crud_queue_log' class='list-group-item'>
-<span class='label label-info'>queue_log</span> - Queue Log</a>
-<a href='?choice=none.crud_quickservers_list' class='list-group-item'>
-<span class='label label-info'>quickservers_list</span> - QuickServers</a>
-<a href='?choice=none.crud_renewals' class='list-group-item'>
-<span class='label label-info'>renewals</span> - Renewals</a>
-<a href='?choice=none.crud_repeat_invoices' class='list-group-item'>
-<span class='label label-info'>repeat_invoices</span> - Repeat Invoices</a>
-<a href='?choice=none.crud_reusable_fantastico' class='list-group-item'>
-<span class='label label-info'>reusable_fantastico</span> - Reusable Fantastico</a>
-<a href='?choice=none.crud_server_actions' class='list-group-item'>
-<span class='label label-info'>server_actions</span> - Recent Server Commands</a>
-<a href='?choice=none.crud_server_billing_stats' class='list-group-item'>
-<span class='label label-info'>server_billing_stats</span> - Server Billing Stats</a>
-<a href='?choice=none.crud_session_log' class='list-group-item'>
-<span class='label label-info'>session_log</span> - session log</a>
-<a href='?choice=none.crud_ssl' class='list-group-item'>
-<span class='label label-info'>ssl</span> - SSL Certificates</a>
-<a href='?choice=none.crud_ssl_list' class='list-group-item'>
-<span class='label label-info'>ssl_list</span> - SSL Certificates</a>
-<a href='?choice=none.crud_templates' class='list-group-item'>
-<span class='label label-info'>templates</span> - Templates</a>
-<a href='?choice=none.crud_user_log' class='list-group-item'>
-<span class='label label-info'>user_log</span> - User Log</a>
-<a href='?choice=none.crud_user_session_activity' class='list-group-item'>
-<span class='label label-info'>user_session_activity</span> - User Session Activity</a>
-<a href='?choice=none.crud_view_invoices2' class='list-group-item'>
-<span class='label label-info'>view_invoices2</span> - View Invoices List</a>
-<a href='?choice=none.crud_view_invoices' class='list-group-item'>
-<span class='label label-info'>view_invoices</span> - View Invoices List</a>
-<a href='?choice=none.crud_vlans' class='list-group-item'>
-<span class='label label-info'>vlans</span> - IP VLAN Manager</a>
-<a href='?choice=none.crud_vps_bandwidth' class='list-group-item'>
-<span class='label label-info'>vps_bandwidth</span> - VPS Bandwidth</a>
-<a href='?choice=none.crud_vps_ips' class='list-group-item'>
-<span class='label label-info'>vps_ips</span> - VPS IP Adddress Space</a>
-<a href='?choice=none.crud_vps' class='list-group-item'>
-<span class='label label-info'>vps</span> - Virtual Private Servers</a>
-<a href='?choice=none.crud_vps_list_free_ips2' class='list-group-item'>
-<span class='label label-info'>vps_list_free_ips2</span> - Free/Available IPs For VPs Servers</a>
-<a href='?choice=none.crud_vps_list_free_ips' class='list-group-item'>
-<span class='label label-info'>vps_list_free_ips</span> - Free/Available IPs For VPs Servers</a>
-<a href='?choice=none.crud_vps_list' class='list-group-item'>
-<span class='label label-info'>vps_list</span> - Virtual Private Servers (VPS)</a>
-<a href='?choice=none.crud_vps_masters' class='list-group-item'>
-<span class='label label-info'>vps_masters</span> - VPS Host Servers</a>
-<a href='?choice=none.crud_vps_next_servers' class='list-group-item'>
-<span class='label label-info'>vps_next_servers</span> - VPS Next Setup Servers</a>
-<a href='?choice=none.crud_webhosting_list' class='list-group-item'>
-<span class='label label-info'>webhosting_list</span> - Website List</a>
-<a href='?choice=none.crud_whm_listaccts' class='list-group-item'>
-<span class='label label-info'>whm_listaccts</span> - Accounts List</a>
-<a href='?choice=none.crud_whos_online' class='list-group-item'>
-<span class='label label-info'>whos_online</span> - Whos Online</a>
+	$functions = get_crud_funcs();
+	page_title('CRUDs List');
+	add_output("
+<style type='text/css'>
+.cruds.list-group {
+	width: 90%;
+	text-align: left;
+}
+.cruds.list-group .list-group-item {
+	display: inline-table;
+	margin: 2px;
+	border-radius: 10px;
+}
+.cruds.list-group .list-group-item.active {
+	display: block;
+}
+</style>");
+	add_output("
+<div class='cruds list-group'>
+	<a href='#' class='list-group-item active'>
+		CRUD Page Links
+		<span class='pull-right label label-danger' style='margin-left: 10px;'>Not Working Yet</span>
+		<span class='pull-right label label-warning' style='margin-left: 10px;'>Loads</span>
+		<span class='pull-right label label-info' style='margin-left: 10px;'>Almost Done</span>
+		<span class='pull-right label label-success' style='margin-left: 10px;'>Ready</span>
+		<span class='pull-right'>Key:</span>
+	</a>");
+	foreach ($functions as $level => $functions_arr) {
+		foreach ($functions_arr as $orig_function => $function_data) {
+			add_output("
+	<a href='?choice=none.{$function_data['function']}' class='list-group-item' target='_blank'>
+		<span class='label label-{$level}'>{$orig_function}</span> - {$function_data['title']}
+	</a>");
+		}
+	}
+	add_output("
 </div>
 ");
+}
+
+function get_crud_funcs() {
+	$functions = array(
+		'success' => array(
+			'admins' => array('function' => 'admins', 'title' => 'Administrator Role Assignments'),
+		),
+		'info' => array(
+			'vps_list' => array('function' => 'vps_list', 'title' => 'VPS List'),
+		),
+		'warning' => array(
+			'backups_list' => array('function' => 'crud_backups_list', 'title' => 'Backup List'),
+			'coupons' => array('function' => 'crud_coupons', 'title' => 'Coupons'),
+			'domains_list' => array('function' => 'crud_domains_list', 'title' => 'Domains'),
+			'modernbill_invoice_list' => array('function' => 'crud_modernbill_invoice_list', 'title' => 'Modernbill Invoice List'),
+			'modernbill_package_list' => array('function' => 'crud_modernbill_package_list', 'title' => 'Modernbill Package Listing'),
+			'ssl_list' => array('function' => 'crud_ssl_list', 'title' => 'SSL Certificates'),
+		),
+		'danger' => array(
+			'abuse' => array('function' => 'crud_abuse', 'title' => 'Abuse'),
+			'active_packages' => array('function' => 'crud_active_packages', 'title' => 'Active Packages'),
+			'admin_tickets' => array('function' => 'crud_admin_tickets', 'title' => 'Admin Tickets'),
+			'admin_tickets_widget' => array('function' => 'crud_admin_tickets_widget', 'title' => 'Admin Tickets'),
+			'backups' => array('function' => 'crud_backups', 'title' => 'Backups'),
+			'customers' => array('function' => 'crud_customers', 'title' => 'Customers'),
+			'dedicated_list' => array('function' => 'crud_dedicated_list', 'title' => 'Dedicated List'),
+			'dns_manager' => array('function' => 'crud_dns_manager', 'title' => 'DNS Manager'),
+			'domains' => array('function' => 'crud_domains', 'title' => 'Domains'),
+			'fantastico_list' => array('function' => 'crud_fantastico_list', 'title' => 'Fantastico License List'),
+			'form_manager' => array('function' => 'crud_form_manager', 'title' => 'Form Manager'),
+			'forum_rss' => array('function' => 'crud_forum_rss', 'title' => 'Latest Forum Posts'),
+			'history_log' => array('function' => 'crud_history_log', 'title' => 'History Log'),
+			'innertell_orders' => array('function' => 'crud_innertell_orders', 'title' => 'Dedicated Server Orders'),
+			'innertell_pending_orders' => array('function' => 'crud_innertell_pending_orders', 'title' => 'Pending Dedicated Server Orders'),
+			'innertell_pending_orders_new' => array('function' => 'crud_innertell_pending_orders_new', 'title' => 'Pending Server Orders'),
+			'innertell_search' => array('function' => 'crud_innertell_search', 'title' => 'Search Results'),
+			'invoices' => array('function' => 'crud_invoices', 'title' => 'Invoices'),
+			'last_logins' => array('function' => 'crud_last_logins', 'title' => 'Last Logn Locations'),
+			'licenses' => array('function' => 'crud_licenses', 'title' => 'Licenses'),
+			'licenses_list' => array('function' => 'crud_licenses_list', 'title' => 'License List'),
+			'modernbill_client_list' => array('function' => 'crud_modernbill_client_list', 'title' => 'Modernbill Client List'),
+			'monitoring_list' => array('function' => 'crud_monitoring_list', 'title' => 'Monitored Systems'),
+			'month_payment_totals' => array('function' => 'crud_month_payment_totals', 'title' => 'Payments This Month'),
+			'new_vps' => array('function' => 'crud_new_vps', 'title' => 'Newest VPS Signups'),
+			'packages' => array('function' => 'crud_packages', 'title' => 'Your Packages'),
+			'paypal_history' => array('function' => 'crud_paypal_history', 'title' => 'Paypal History'),
+			'paypal_transactions' => array('function' => 'crud_paypal_transactions', 'title' => 'Paypal Transactions'),
+			'pending_vps_list' => array('function' => 'crud_pending_vps_list', 'title' => 'Pending Virtual Private Servers (VPS)'),
+			'queue_log' => array('function' => 'crud_queue_log', 'title' => 'Queue Log'),
+			'quickservers_list' => array('function' => 'crud_quickservers_list', 'title' => 'QuickServers'),
+			'renewals' => array('function' => 'crud_renewals', 'title' => 'Renewals'),
+			'repeat_invoices' => array('function' => 'crud_repeat_invoices', 'title' => 'Repeat Invoices'),
+			'reusable_fantastico' => array('function' => 'crud_reusable_fantastico', 'title' => 'Reusable Fantastico'),
+			'server_actions' => array('function' => 'crud_server_actions', 'title' => 'Recent Server Commands'),
+			'server_billing_stats' => array('function' => 'crud_server_billing_stats', 'title' => 'Server Billing Stats'),
+			'session_log' => array('function' => 'crud_session_log', 'title' => 'session log'),
+			'ssl' => array('function' => 'crud_ssl', 'title' => 'SSL Certificates'),
+			'templates' => array('function' => 'crud_templates', 'title' => 'Templates'),
+			'user_log' => array('function' => 'crud_user_log', 'title' => 'User Log'),
+			'user_session_activity' => array('function' => 'crud_user_session_activity', 'title' => 'User Session Activity'),
+			'view_invoices2' => array('function' => 'crud_view_invoices2', 'title' => 'View Invoices List'),
+			'view_invoices' => array('function' => 'crud_view_invoices', 'title' => 'View Invoices List'),
+			'vlans' => array('function' => 'crud_vlans', 'title' => 'IP VLAN Manager'),
+			'vps_bandwidth' => array('function' => 'crud_vps_bandwidth', 'title' => 'VPS Bandwidth'),
+			'vps_ips' => array('function' => 'crud_vps_ips', 'title' => 'VPS IP Adddress Space'),
+			'vps' => array('function' => 'crud_vps', 'title' => 'Virtual Private Servers'),
+			'vps_list_free_ips2' => array('function' => 'crud_vps_list_free_ips2', 'title' => 'Free/Available IPs For VPs Servers'),
+			'vps_list_free_ips' => array('function' => 'crud_vps_list_free_ips', 'title' => 'Free/Available IPs For VPs Servers'),
+			'vps_list' => array('function' => 'crud_vps_list', 'title' => 'Virtual Private Servers (VPS)'),
+			'vps_masters' => array('function' => 'crud_vps_masters', 'title' => 'VPS Host Servers'),
+			'vps_next_servers' => array('function' => 'crud_vps_next_servers', 'title' => 'VPS Next Setup Servers'),
+			'webhosting_list' => array('function' => 'crud_webhosting_list', 'title' => 'Website List'),
+			'whm_listaccts' => array('function' => 'crud_whm_listaccts', 'title' => 'Accounts List'),
+			'whos_online' => array('function' => 'crud_whos_online', 'title' => 'Whos Online'),
+		),
+	);
+	return $functions;
 }
