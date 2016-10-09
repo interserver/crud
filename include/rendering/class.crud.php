680f17e939e2bf1f0252061eb649d6e20d56d68d
Joe Huss <detain@interserver.net>
2016-10-09 06:44:53 +0000

    fix minor variable referece and dropped storing the crud obj as a variable since its never used
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@158 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/rendering/class.crud.php
+++ include/rendering/class.crud.php
@@ -1770,7 +1770,7 @@
 						$this->add_filter_link($field, "?choice=none.view_host_server&module={$this->module}&name=%{$this->settings['PREFIX']}_name%", 'View Host Server', 'view_service');
 						break;
 					case $this->settings['TITLE_FIELD']:
-						if ($module != 'webhosting')
+						if ($this->module != 'webhosting')
 							$this->add_filter_link($field, '?choice=none.view_'.$this->settings['PREFIX'].'&id=%'.$this->settings['PREFIX'].'_id%', 'View '.$this->settings['TITLE'], 'view_service');
 						elseif ($GLOBALS['tf']->ima == 'admin')
 							$this->add_filter_link($field, '?choice=none.view_'.$this->settings['PREFIX'].'2&id=%'.$this->settings['PREFIX'].'_id%', 'View '.$this->settings['TITLE'], 'view_service');
