609f383a23510033826da3154f7347d283f45c0c
Joe Huss <detain@interserver.net>
2016-10-10 13:37:12 +0000

    - removed 2 functions from crud.js not in use by the crud system that were leftover  - added phpdoc style comment blocks to a handful of the functions in the crud JS file
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@191 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- public_html/js/crud.js
+++ public_html/js/crud.js
@@ -1,153 +1,11 @@
-function crud_get_order_row(order) {
-	var html;
-	html = '\
-		<tr data-module="'+order.module+'" data-id="'+order.order_id+'" data-status="'+order.order_status+'">\
-			<td>\
-						'+order.tblname+' Order ID <a href="index.php?choice=none.view_website2&id='+order.order_id+'" title="View Website Order" target="_blank"><span class="badge">'+order.order_id+'</span></a><br>\
-						<a href="http://'+order.title_field+'" title="Goto Website" target="_blank">'+order.title_field+'</a><br>\
-						Status\
-						';
-	if (order.order_status == 'active')
-		html = html + '<span class="label label-success">'+order.order_status+'</span><br>';
-	else if (order.order_status == 'pending')
-		html = html + '<span class="label label-info">'+order.order_status+'</span><br>';
-	else if (order.order_status == 'pendapproval')
-		html = html + '<span class="label label-primary">'+order.order_status+'</span><br>';
-	else if (order.order_status == 'expired')
-		html = html + '<span class="label label-warning">'+order.order_status+'</span><br>';
-	else if (order.order_status == 'canceled')
-		html = html + '<span class="label label-danger">'+order.order_status+'</span><br>';
-	else
-		html = html + '<span class="label label-default">'+order.order_status+'</span><br>';
-	html = html + '\
-						'+order.services_name+'<br>\
-						';
-	if (order.repeat_invoices_frequency == '')
-		html = html + 'Term Unknown<br>';
-	else if (order.repeat_invoices_frequency == 1)
-		html = html + 'Term '+order.repeat_invoices_frequency+' Month<br>';
-	else
-		html = html + 'Term '+order.repeat_invoices_frequency+' Months<br>';
-	html = html + 'Ordered: ' + order.order_date;
-	html = html + '\
-			</td>\
-			<td>\
-						<a href="index.php?choice=none.edit_customer3&customer='+order.account_id+'" title="Edit Customer" target="_blank"><i class="fa fa-envelope"></i> '+order.account_lid+'</a><br>\
-						<address>';
-	if (order.name != '' && typeof order.name != 'object')
-		html = html + '\
-							<i class="fa fa-user"></i> '+order.name+'<br>';
-	if (order.address != '' && typeof order.address != 'object')
-		html = html + '\
-							<i class="fa fa-road"></i> '+wordwrap(order.address, 30, '<br/>\n<i class="fa fa-empty" ></i> ', false)+'<br>';
-	if (order.address2 != '' && order.address2 != '-' && typeof order.address2 != 'object')
-		html = html + '\
-							<i class="fa fa-empty" ></i> '+order.address2+'<br>';
-	if (order.city != '' && typeof order.city != 'object')
-		html = html + '\
-							<i class="fa fa-empty"></i> '+order.city+', '+order.state+' '+order.zip+'<br>';
-	if (order.country != '' && typeof order.country != 'object') {
-		var country=String(order.country);
-		html = html + '\
-							<i class="fa fa-empty"></i> '+order.country+' <img class="flag" src="/images/flags/'+country.toLowerCase()+'.png" alt="'+order.country+'"><br>';
-	}
-	html = html + '\
-						</address>\
-			</td>\
-			<td>\
-						Payment Method '+order.payment_method+'<br>\
-						';
-	if (order.invoices_type == 10)
-		html = html + '<a onclick="modal_transaction(\''+order.invoices_description+'\'); return false;" title="View PayPal Transaction"><i class="fa fa-paypal"></i> '+order.invoices_description+'</a><br>';
-	else if (order.invoices_type == 11)
-		html = html + '<i class="fa fa-credit-card"></i> '+order.cc+'</a><br>';
-	else if (order.cc != '' && typeof order.cc != 'object')
-		html = html + '<i class="fa fa-credit-card"></i> '+order.cc+'</a><br>';
-	if (order.maxmind_riskscore == '' || typeof order.maxmind_riskscore == 'object')
-		html = html + '\
-						Fraud Risk Unknown <a href="index.php?choice=none.view_maxmind&customer='+order.account_id+'" title="Refresh MaxMind Score" target="_blank"><i class="fa fa-refresh"></i></a><br>';
-	else
-		html = html + '\
-						Fraud Risk '+order.maxmind_riskscore+'% <a onclick="modal_maxmind('+order.account_id+'); return false;" title="View MaxMind Report"><i class="fa fa-search"></i></a><br>';
-	if (typeof order.invoices_amount != "undefined" && order.invoices_amount != null && order.invoices_amount != "")
-		html = html + 'Paid $'+order.invoices_amount+'<br>';
-	if (typeof order.cc_response != "undefined" && order.cc_response != null && order.cc_response != "")
-		html = html + 'CC Response:'+order.cc_response+'<br>';
-	if (typeof order.coupon != "undefined" && order.coupon != null && order.coupon != "")
-		html = html + 'Coupon:'+order.coupon+'<br>';
-	html = html + '\
-			</td>\
-			<td>';
-	if ((order.order_status == 'pending' || order.order_status == 'pendapproval') && order.cc != '' && typeof order.cc != 'object')
-		html = html + '\
-						<a onclick="approval_handler(\'activate\', '+order.order_id+', \''+order.module+'\'); return false;" class="btn btn-default" title="Enable Credit-Card Use on this account as needed and then proceeding to attempt charging the card to activate the order.">\
-							<span class="fa-stack fa-lg">\
-								<i class="fa fa-credit-card fa-stack-1x"></i>\
-								<i class="fa fa-check fa-stack-1x text-success"></i>\
-							</span>\
-							Enable Credit-Card + Activate\
-						</a><br>';
-	else if (order.payment_method == 'cc')
-		html = html + '\
-						<a onclick="approval_handler(\'cancel\', '+order.order_id+', \''+order.module+'\'); return false;" class="btn btn-default" title="Cancel this order and disable CC use for this customer.">\
-							<span class="fa-stack fa-lg">\
-								<i class="fa fa-credit-card fa-stack-1x"></i>\
-								<i class="fa fa-close fa-stack-1x text-danger"></i>\
-							</span>\
-							Disable Credit-Card + Cancel\
-						</a><br>';
-	else if (order.invoices_type != '' && typeof order.invoices_type != 'object')
-		html = html + '\
-						<a onclick="approval_handler(\'cancel\', '+order.order_id+', \''+order.module+'\'); return false;" class="btn btn-default" title="Cancel this order.">\
-							<span class="fa-stack fa-lg">\
-								<i class="fa fa-close fa-stack-1x text-danger"></i>\
-							</span>\
-							Cancel\
-						</a><br>';
-	if (order.account_status != 'active')
-		html = html + '\
-						<a onclick="approval_handler(\'enable_account\', '+order.order_id+', \''+order.module+'\'); return false;" class="btn btn-default" title="Enable this clients account.">\
-							<span class="fa-stack fa-lg">\
-								<i class="fa fa-user fa-stack-1x"></i>\
-								<i class="fa fa-check fa-stack-1x text-success"></i>\
-							</span>\
-							Enable Account\
-						</a><br>';
-	else
-		html = html + '\
-						<a onclick="approval_handler(\'disable_account\', '+order.order_id+', \''+order.module+'\'); return false;" class="btn btn-default" title="Disable this clients account.">\
-							<span class="fa-stack fa-lg">\
-								<i class="fa fa-user fa-stack-1x"></i>\
-								<i class="fa fa-close fa-stack-1x text-danger"></i>\
-							</span>\
-							Disable Account\
-						</a><br>';
-	if (order.invoices_type != '' && typeof order.invoices_type != 'object') {
-		if (order.payment_method == 'cc')
-		html = html + '\
-						<a onclick="approval_handler(\'refund_cc\', '+order.order_id+', \''+order.module+'\'); return false;" class="btn btn-default" title="Refund this clients creditcard charge for this order.">\
-							<span class="fa-stack fa-lg">\
-								<i class="fa fa-credit-card fa-stack-1x"></i>\
-								<i class="fa fa-undo fa-stack-1x text-danger"></i>\
-							</span>\
-							Refund Credit Card\
-						</a><br>';
-		else
-		html = html + '\
-						<a onclick="approval_handler(\'refund_paypal\', '+order.order_id+', \''+order.module+'\'); return false;" class="btn btn-default" title="Refund this PayPal transaction.">\
-							<span class="fa-stack fa-lg">\
-								<i class="fa fa-paypal fa-stack-1x"></i>\
-								<i class="fa fa-undo fa-stack-1x text-danger"></i>\
-							</span>\
-							Refund PayPal\
-						</a><br>';
-	}
-	html = html + '\
-			</td>\
-		</tr>';
-	return html;
-}
-
+/**
+ * processing submit buttons and handles the responses
+ *
+ * @param what the form your submitting (ie edit, add, delete)
+ * @param that the this object that triggered the call
+ *
+ * @returns {Boolean}
+ */
 function crud_submit_handler(what, that) {
 	var disabled = jQuery("#"+what+"ModalForm input[disabled], #"+what+"ModalForm select[disabled]");
 	disabled.removeAttr("disabled");
@@ -192,6 +50,11 @@ function crud_submit_handler(what, that) {
 	return false;
 }
 
+/**
+ * triggers the edit form to show up for a given row and populates the form with the rowws data
+ *
+ * @param that the this object from the row that triggered the call
+ */
 function crud_edit_form(that) {
 	var parent = get_crud_row_idx(that);
 	//console.log(get_crud_row_id(that));
@@ -205,6 +68,11 @@ function crud_edit_form(that) {
 	jQuery("#editModal").modal("show");
 }
 
+/**
+ * triggers the delete  form to show up for a given row and populates the form with the rowws data
+ *
+ * @param that the this object from the row that triggered the call
+ */
 function crud_delete_form(that) {
 	var parent = get_crud_row_id(that);
 	var row = crud_rows[parent], field, value;
@@ -215,28 +83,20 @@ function crud_delete_form(that) {
 	jQuery("#deleteModal").modal("show");
 }
 
-function crud_approval_list(status, offset, limit) {
-	if (typeof jQuery('#order_status_grp .btn.active').attr('data-target') != "undefined" && typeof status == "undefined")
-		status = jQuery('#order_status_grp .btn.active').attr('data-target');
-	var url = "ajax_pending_approval.php?action=list&status="+status;
-	if (typeof offset != "undefined")
-		url = url+"&offset="+offset;
-	url = url + "&limit=" + document.getElementById('pending_approval_limit').value;
-	url = url + "&module=" + document.getElementById('pending_approval_module').value;
-	$.getJSON(url, { }, function(json) {
-		jQuery('table.orders tbody').html('');
-		//console.log(json);
-		for (var x = 0; x < json.orders.length; x++)
-			jQuery('table.orders tbody').append(crud_get_order_row(json.orders[x]));
-		$('.section a[title]').tooltip();
-	});
-	return false;
-}
-
+/**
+ * gets the row index on the table / array
+ *
+ * @param that the this object for the given row
+ */
 function get_crud_row_idx(that) {
 	return replaceAll(jQuery(that).parent().parent().attr("id"), "itemrow", "");
 }
 
+/**
+ * gets the row primary key id on the table / array
+ *
+ * @param that the this object for the given row
+ */
 function get_crud_row_id(that) {
 	var parent = get_crud_row_idx(that);
 	var row = crud_rows[parent], field, value;
