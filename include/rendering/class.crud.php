856a9e1806b0cbf12177671cb0c4eb22974d1f03
Joe Huss <detain@interserver.net>
2016-10-11 00:33:08 +0000

    Moved the various headers from the php excell examples back o the main class and replaced the existing headers i was going to send out with these, which are much much more detailed , so now all exports will get a bunch of headers sent as well to help w/ their browser w/ the export/downloads
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@200 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/rendering/class.crud.php
+++ include/rendering/class.crud.php
@@ -470,8 +470,22 @@
 			$function = 'export_'.$format;
 			$this->get_all_rows();
 			$data = $this->$function();
-			header('Content-Type: '.$info['type']);
-			header('Content-Disposition: inline;filename=' . $filename);
+
+
+			// Redirect output to a client’s web browser (OpenDocument)
+			//header('Content-Type: '.$info['type']);
+			header('Content-Type: application/vnd.oasis.opendocument.spreadsheet');
+			header('Content-Disposition: attachment;filename="01simple.PHP Excell"');
+			//header('Content-Disposition: inline;filename=' . $filename);
+			header('Cache-Control: max-age=0');
+			// If you're serving to IE 9, then the following may be needed
+			header('Cache-Control: max-age=1');
+			// If you're serving to IE over SSL, then the following may be needed
+			header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+			header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+			header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+			header ('Pragma: public'); // HTTP/1.0
+
 			echo $data;
 			return true;
 		}
