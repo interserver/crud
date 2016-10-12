77286bc546f2b2d7f993397578b2e9c3d595b83d
Joe Huss <detain@interserver.net>
2016-10-12 00:29:51 +0000

    fixing the one sql query so it doesnt list some types of domains twice while still showing domains with no main a record
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@209 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/crud/crud_dns_manager.php
+++ include/crud/crud_dns_manager.php
@@ -15,7 +15,7 @@
  */
 function crud_dns_manager() {
 	require_once(INCLUDE_ROOT . '/rendering/class.crud.php');
-	crud::init("select domains.id, domains.account, domains.name, records.content from domains left join records on domains.id=records.domain_id where ((records.type='A' and (records.name=domains.name or records.name='') ) or records.type is null)", 'pdns')
+	crud::init("select domains.id, domains.account, domains.name, records.content from domains left join records on domains.id=records.domain_id and records.type='A' and (records.name=domains.name or records.name='')", 'pdns')
 		->set_title("DNS Manager")
 		->go();
 }
