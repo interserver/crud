b67919c0f38b39b9d711f68d9e667f9802399790
Joe Huss <detain@interserver.net>
2016-10-18 07:55:04 +0000

    adding menu links for the same domain accounts function changed the same domain accoutns function to use a tooltip instead of title over the edit customer link
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@223 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/crud/same_domain_accounts.php
+++ include/crud/same_domain_accounts.php
@@ -21,7 +21,7 @@ function same_domain_accounts() {
 		->disable_delete()
 		->disable_edit()
 		->enable_fluid_container()
-		->add_row_button('<button type="button" class="btn btn-primary btn-xs" onclick="window.location=\'index.php?choice=none.edit_customer3&customer=\'+get_crud_row_id(this);" title="Goto Edit Customer" data-title="Goto Edit Customer"><i class="fa fa-fw fa-user"></i></button>')
+		->add_row_button('<button type="button" class="btn btn-primary btn-xs" onclick="window.location=\'index.php?choice=none.edit_customer3&customer=\'+get_crud_row_id(this);" title="Goto Edit Customer" data-toggle="tooltip" tooltip="Goto Edit Customer"><i class="fa fa-fw fa-user"></i></button>')
 		->set_extra_url_args('&id='.$id)
 		->go();
 }
