11f385747d77a222dcb2d7c80dd52ef9270a49c1
Joe Huss <detain@interserver.net>
2016-11-02 16:41:47 +0000

    fixing a problem in view_invoices
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@253 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/crud/crud_view_invoices.php
+++ include/crud/crud_view_invoices.php
@@ -15,7 +15,7 @@
  */
 function crud_view_invoices() {
 	require_once(INCLUDE_ROOT . '/rendering/class.crud.php');
-	crud::init("select date_format(invoices_date, '%b %e, %Y'), invoices_type, __TITLE_FIELD__ as service, invoices_description,invoices_custid,invoices_amount,invoices_paid,invoices_group,invoices_extra,invoices_module,invoices_due_date,invoices_service,invoices_id from invoices left join __TABLE__ on (invoices_service=__PREFIX___id or (invoices_service=0 and invoices_type=1 and (__PREFIX___invoice=invoices_extra or invoices_description like concat('%for __TBLNAME__ ', __PREFIX___id)))) where invoices_module='__MODULE__'")
+	crud::init("select date_format(invoices_date, '%b %e, %Y'), invoices_type, __TITLE_FIELD__ as service, invoices_description,invoices_custid,invoices_amount,invoices_paid,invoices_group,invoices_extra,invoices_module,invoices_due_date,invoices_service,invoices_id from invoices left join __TABLE__ on invoices_service=__PREFIX___id where invoices_module='__MODULE__'")
 		->set_title('View Invoices List')
 		->go();
 }
