4b107d5f715906c5451c8b34482b71244e40f34b
Joe Huss <detain@interserver.net>
2016-11-03 08:16:17 +0000

    rewrite the monthly payment totals as a single query
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@254 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/crud/crud_month_payment_totals.php
+++ include/crud/crud_month_payment_totals.php
@@ -14,15 +14,8 @@
  * @return void
  */
 function crud_month_payment_totals() {
-	add_output(alert('TODO', 'Have it work over all modules'));
 	require_once(INCLUDE_ROOT . '/rendering/class.crud.php');
-	crud::init("select '__TBLNAME__' as module, sum(invoices.invoices_amount) AS invoices_total FROM
-  invoices
-WHERE
-  year(invoices.invoices_date) = year(now())
-  AND month(invoices.invoices_date) = month(now())
-  AND invoices.invoices_type >= 10
-  AND invoices.invoices_module='__MODULE__'")
+	crud::init("select invoices_module as module, sum(invoices_amount) as invoices_total from invoices where year(invoices.invoices_date) = year(now()) AND month(invoices.invoices_date) = month(now()) AND invoices.invoices_type >= 10 group by invoices_module")
 		->set_title('Payments This Month')
 		->go();
 }
