cf92da2f5d309e5f3c9649e7713e7391a206a32b
Joe Huss <detain@interserver.net>
2017-01-30 12:36:23 +0000

    dropped host server link for non admins in the crud class default filters
    git-svn-id: https://creation.interserver.net/svn/crud/trunk@276 9440e491-775b-45db-8ed1-aeac5ee8fee9

--- include/rendering/class.crud.php
+++ include/rendering/class.crud.php
@@ -2430,7 +2430,7 @@
 					$this->add_filter_link($field, '?choice=none.edit_customer3&customer=%account_id%', 'Edit Customer', 'view_customer');
 				/*} elseif ($field == 'invoices_paid') {
 					$this->add_filter($field, array('1' => '<i class="fa fa-fw fa-check"></i>', '2' => '<i class="fa fa-fw fa-times"></i>'), 'simple');*/
-				} elseif ($field == $this->settings['PREFIX'].'_name') {
+				} elseif ($field == $this->settings['PREFIX'].'_name' && $this->admin == true) {
 					$this->add_filter_link($field, "?choice=none.view_host_server&module={$this->module}&name=%{$this->settings['PREFIX']}_name%", 'View Host Server', 'view_service');
 				/*} elseif ($field == $this->settings['PREFIX'].'_id') {
 					// @TODO distinguish between like vps_masters.vps_id and vps.vps_id type fields before doing this*/
