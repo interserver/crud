0b2ed001095c8a28d84fffb6373be85995615dad
Joe Huss <detain@interserver.net>
2017-07-08 22:52:34 -0400

    removing the @version phpdoc tag

--- src/crud/crud_domains_list.php
+++ src/crud/crud_domains_list.php
@@ -3,7 +3,6 @@
  * Domains List
  * Last Changed: $LastChangedDate: 2016-10-05 12:42:23 -0400 (Wed, 05 Oct 2016) $
  * @author detain
- * @version $Revision: 21022 $
  * @copyright 2017
  * @package MyAdmin
  * @subpackage Domains
