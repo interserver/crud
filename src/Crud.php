a25df5284dd57f1124962ceb7f5aa267ea644492
Ganesh <kumar.interserver@gmail.com>
2017-08-02 09:26:42 -0400

    Making 'active' as a default

--- src/Crud.php
+++ src/Crud.php
@@ -985,7 +985,7 @@ class Crud
 	 * @return string the sql string to add to the query
 	 */
 	public function search_to_sql() {
-		$search = [];
+		$search = ['active'];
 		$valid_opers = ['=', 'in'];
 		$implode_type = 'and';
 		//$this->log('Search Terms: '.json_encode($this->search_terms), __LINE__, __FILE__, 'debug');
