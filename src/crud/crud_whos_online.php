8fb194649a92d42957f833ed39b4c8a2fbbec1e3
Joe Huss <detain@interserver.net>
2018-06-16 21:18:39 -0400

    updates to the whos online widget replacing the query

--- src/crud/crud_whos_online.php
+++ src/crud/crud_whos_online.php
@@ -14,7 +14,15 @@ use \MyCrud\Crud;
  */
 function crud_whos_online() {
 	add_output(alert('TODO', 'Hide some of the fields, make it auto update'));
-		Crud::init("select history_id, session_owner, account_lid,  unix_timestamp() - unix_timestamp(history_timestamp) as session_idle, history_type, session_ip, session_id, session_login from sessions, session_log, accounts where history_sid=session_id and history_owner=account_id and account_ima != 'services' and history_timestamp >= date_sub(now(), INTERVAL 3 HOUR)")
+		Crud::init("SELECT history_owner custid, account_lid AS email, DATE_FORMAT(history_timestamp, '%r') AS time, history_type AS page, access_ip AS ip
+FROM session_log
+	LEFT JOIN access_log ON history_sid = access_sid
+	LEFT JOIN sessions ON history_sid = session_id
+	LEFT JOIN accounts ON history_owner = account_id
+WHERE session_id IS NOT NULL
+AND account_id IS NOT NULL
+AND access_id IS NOT NULL")
+		->set_order('history_id','desc')
 		->set_title('Whos Online')
 		->go();
 }
