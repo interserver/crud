546e4f2f85fb57dd84d7577b6187d14a3501adb7
Ganesh <kumar.interserver@gmail.com>
2018-09-27 09:20:33 -0400

    Domain list page upgrade - minor coding standard updates

--- src/crud/crud_domains_list.php
+++ src/crud/crud_domains_list.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * Domains List.
+ *
  * @author Joe Huss <detain@interserver.net>
  * @copyright 2018
- * 
+ *
  * @category Domains
  */
 use MyCrud\Crud;
@@ -15,20 +16,20 @@ use MyCrud\Crud;
  */
 function crud_domains_list()
 {
-	$module = 'domains';
-	$settings = \get_module_settings($module);
-	page_title($settings['TITLE'].' List');
-	Crud::init("select concat('<a href=\"index.php?choice=none.view_domain&id=',{$settings['PREFIX']}_id,'\"><img src=\"http://shot.sh?w=300&h=100&img=',{$settings['PREFIX']}_hostname,'\"></a>') as screenshot,{$settings['PREFIX']}_id,{$settings['PREFIX']}_hostname,{$settings['PREFIX']}_cost,{$settings['PREFIX']}_status from {$settings['TABLE']}", $module)
-		->set_order($settings['PREFIX'].'_status', 'asc')
-		->set_title($settings['TITLE'].' List')
-		->add_header_button($GLOBALS['tf']->link('index.php', 'choice=none.buy_new_'.$settings['PREFIX']), 'Order', 'primary', 'shopping-cart', 'Order '.$settings['TITLE'], 'client')
-		->add_title_search_button([[$settings['PREFIX'].'_status', '=', 'active']], 'Active', 'info active')
-		->add_title_search_button([[$settings['PREFIX'].'_status', 'in', ['pending', 'pending-setup', 'pend-approval']]], 'Pending', 'info')
-		->add_title_search_button([[$settings['PREFIX'].'_status', 'in', ['canceled', 'expired']]], 'Expired', 'info')
-		->add_title_search_button([], 'All', 'info')
-		->disable_delete()
-		->disable_edit()
-		->enable_fluid_container()
-		->add_row_button('none.view_'.$settings['PREFIX'].($module == 'webhosting' ? ($GLOBALS['tf']->ima == 'admin' ? '2' : '4') : '').'&id=%id%', 'View '.$settings['TITLE'], 'primary', 'cog')
-		->go();
+    $module = 'domains';
+    $settings = \get_module_settings($module);
+    page_title($settings['TITLE'].' List');
+    Crud::init("select concat('<a href=\"index.php?choice=none.view_domain&id=',{$settings['PREFIX']}_id,'\"><img src=\"http://shot.sh?w=300&h=100&img=',{$settings['PREFIX']}_hostname,'\"></a>') as screenshot,{$settings['PREFIX']}_id,{$settings['PREFIX']}_hostname,{$settings['PREFIX']}_cost,{$settings['PREFIX']}_status from {$settings['TABLE']}", $module)
+    ->set_order($settings['PREFIX'].'_status', 'asc')
+    ->set_title($settings['TITLE'].' List')
+    ->add_header_button($GLOBALS['tf']->link('index.php', 'choice=none.buy_new_'.$settings['PREFIX']), 'Order', 'primary', 'shopping-cart', 'Order '.$settings['TITLE'], 'client')
+    ->add_title_search_button([[$settings['PREFIX'].'_status','=','active']], 'Active', 'info active')
+    ->add_title_search_button([[$settings['PREFIX'].'_status','in',['pending','pending-setup','pend-approval']]], 'Pending', 'info')
+    ->add_title_search_button([[$settings['PREFIX'].'_status','in',['canceled','expired']]], 'Expired', 'info')
+    ->add_title_search_button([], 'All', 'info')
+    ->disable_delete()
+    ->disable_edit()
+    ->enable_fluid_container()
+    ->add_row_button('none.view_'.$settings['PREFIX'].($module == 'webhosting' ? ($GLOBALS['tf']->ima == 'admin' ? '2' : '4') : '').'&id=%id%', 'View '.$settings['TITLE'], 'primary', 'cog')
+    ->go();
 }
