ae34e382b2f28d185422090fa353c5d2082c71a3
Joe Huss <detain@interserver.net>
2018-12-05 05:02:17 -0500

    more crud updates this time setting labels

--- src/crud/crud_paypal_history.php
+++ src/crud/crud_paypal_history.php
@@ -16,6 +16,7 @@ function crud_paypal_history()
 {
 	Crud::init('SELECT accounts.account_id AS owner, paypal.txn_id AS txn_id, paypal.payment_status AS payment_status, paypal.`when` AS history_timestamp, paypal.payer_email AS payer_email, paypal.payment_gross AS payment_gross FROM paypal INNER JOIN accounts ON paypal.lid = accounts.account_lid')
 		->set_title(_('Paypal History'))
+		->set_labels(['txn_id' => 'Transaction ID', 'payment_status' => 'Status', 'history_timestamp' => 'When', 'payer_email' => 'Email', 'payment_gross' => 'Amount'])
 		->set_order('history_timestamp', 'desc')
 		->go();
 }
