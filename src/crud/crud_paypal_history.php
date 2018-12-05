d6286d37f83d01d92f886bc750a5aaa3e5caebd1
Joe Huss <detain@interserver.net>
2018-12-05 04:59:22 -0500

    updates to crud paypal hsitory

--- src/crud/crud_paypal_history.php
+++ src/crud/crud_paypal_history.php
@@ -14,7 +14,8 @@ use \MyCrud\Crud;
  */
 function crud_paypal_history()
 {
-	Crud::init('SELECT accounts.account_id AS owner, paypal.txn_id AS txn_id, paypal.payment_status AS payment_status, paypal.`when` AS history_timestamp, paypal.payer_email AS payer_email, paypal.payment_gross AS payment_gross FROM paypal INNER JOIN accounts ON paypal.lid = accounts.account_lid ORDER BY paypal.`when` DESC')
+	Crud::init('SELECT accounts.account_id AS owner, paypal.txn_id AS txn_id, paypal.payment_status AS payment_status, paypal.`when` AS history_timestamp, paypal.payer_email AS payer_email, paypal.payment_gross AS payment_gross FROM paypal INNER JOIN accounts ON paypal.lid = accounts.account_lid')
 		->set_title(_('Paypal History'))
+		->set_order('history_timestamp', 'desc')
 		->go();
 }
