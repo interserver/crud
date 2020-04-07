fd966c2e3242b4f9fe530a7257b52dcff79329fd
Joe Huss <detain@interserver.net>
2020-04-07 11:11:16 -0400

    updates changing the display for coupons

--- src/crud/crud_coupons.php
+++ src/crud/crud_coupons.php
@@ -14,14 +14,14 @@ use \MyCrud\Crud;
  */
 function crud_coupons()
 {
-    page_title(_('Coupons List'));
-    function_requirements('has_acl');
-    if ($GLOBALS['tf']->ima != 'admin' || !has_acl('client_billing')) {
-        dialog(_('Not Admin'), _('Not Admin or you lack the permissions to view this page.'));
-        return false;
-    }
-    // IF(accounts_ext.account_id is null,'<i class=\"fa fa-remove\">',concat('<a href=\"index.php?choice=none.edit_customer&customer=',accounts_ext.account_id,'\"><i class=\"fa fa-search\"></i></a>')) AS affiliate
-    // LEFT JOIN accounts_ext ON account_key = 'referrer_coupon' AND account_value = name
+	page_title(_('Coupons List'));
+	function_requirements('has_acl');
+	if ($GLOBALS['tf']->ima != 'admin' || !has_acl('client_billing')) {
+		dialog(_('Not Admin'), _('Not Admin or you lack the permissions to view this page.'));
+		return false;
+	}
+	// IF(accounts_ext.account_id is null,'<i class=\"fa fa-remove\">',concat('<a href=\"index.php?choice=none.edit_customer&customer=',accounts_ext.account_id,'\"><i class=\"fa fa-search\"></i></a>')) AS affiliate
+	// LEFT JOIN accounts_ext ON account_key = 'referrer_coupon' AND account_value = name
 	Crud::init("SELECT
 id, 
 name,
@@ -34,17 +34,17 @@ IF(applies = -1,'"._('All Packages')."',IF(applies = 0,'"._('No Packages')."',ap
 module
 FROM coupons 
 LEFT JOIN accounts ON accounts.account_id = customer
-where name not in (select account_value from accounts_ext where account_key='referrer_coupon')
+where amount=0.01
 ")
 		->set_title(_('Coupons'))
-        ->enable_labels()
-        ->set_labels(['id' => _('ID'),'name' => _('Coupon Name'), 'amount' => _('Amount'), 'customer' => _('Customer'), 'usable' => _('Usable'), 'applies' => _('Applies'), 'type' => 'Type', 'onetime' => 'One Time', 'module' => 'Module', 'affiliate' => 'Affiliate'])
-        ->add_header_button($GLOBALS['tf']->link('index.php', 'choice=none.coupons'), _('Manage Coupons'), 'primary', 'pencil', _('Manage Coupons'))
-        ->set_order('id', 'asc')
-        ->add_title_search_button([['onetime','=',1],['usable','=',1],['amount','=',0.01]], _('Affiliate Coupons'), 'info')
-        ->add_title_search_button([], _('All'), 'info active')
-        ->disable_delete()
-        ->disable_edit()
-        ->enable_fluid_container()
+		->enable_labels()
+		->set_labels(['id' => _('ID'),'name' => _('Coupon Name'), 'amount' => _('Amount'), 'customer' => _('Customer'), 'usable' => _('Usable'), 'applies' => _('Applies'), 'type' => 'Type', 'onetime' => 'One Time', 'module' => 'Module', 'affiliate' => 'Affiliate'])
+		->add_header_button($GLOBALS['tf']->link('index.php', 'choice=none.coupons'), _('Manage Coupons'), 'primary', 'pencil', _('Manage Coupons'))
+		->set_order('id', 'asc')
+		->add_title_search_button([['onetime','=',1],['usable','=',1],['amount','=',0.01]], _('Affiliate Coupons'), 'info')
+		->add_title_search_button([], _('All'), 'info active')
+		->disable_delete()
+		->disable_edit()
+		->enable_fluid_container()
 		->go();
 }
