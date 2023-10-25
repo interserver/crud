ddbb123d263d5dd9c2db8befa5757a717479ca4f
Ganesh R <kumar.interserver@gmail.com>
2023-10-25 18:30:33 +0530

    Dedicated List: Removing client column from clientside

--- src/crud/crud_dedicated_list.php
+++ src/crud/crud_dedicated_list.php
@@ -14,12 +14,18 @@ use \MyCrud\Crud;
  */
 function crud_dedicated_list()
 {
-	Crud::init('select server_id, account_lid, primary_ipv4, server_hostname, server_status from servers left join accounts on account_id=server_custid left join assets on order_id=server_id', 'servers')
+	$query = "SELECT server_id, primary_ipv4, server_hostname, server_status FROM servers LEFT JOIN assets ON order_id=server_id";
+	$labels = ['server_id' => _('ID'), 'primary_ipv4' => 'IP', 'server_hostname' =>  _('Server Name'), 'server_status' => _('Status')];
+	if ($GLOBALS['tf']->ima == 'admin') {
+		$query = "SELECT server_id, account_lid, primary_ipv4, server_hostname, server_status FROM servers LEFT JOIN accounts on account_id=server_custid LEFT JOIN assets ON order_id=server_id";
+		$labels = ['server_id' => _('ID'),'account_lid' => _('Client'), 'primary_ipv4' => 'IP', 'server_hostname' =>  _('Server Name'), 'server_status' => _('Status')];
+	}
+	Crud::init($query, 'servers')
         ->set_limit_custid_role('list_all')
 		->set_title(_('Dedicated List'))
 		->set_order('server_id', 'desc')
 		->enable_labels()
-		->set_labels(['server_id' => _('ID'),'account_lid' => _('Client'), 'primary_ipv4' => 'IP', 'server_hostname' =>  _('Server Name'), 'server_status' => _('Status')])
+		->set_labels($labels)
 		->add_header_button($GLOBALS['tf']->link('index.php', 'choice=none.order_server'), _('Order'), 'primary', 'shopping-cart', _('Order Server'), 'client')
 		->add_title_search_button([['server_status','in',['active','active-billing']]], _('Active'), 'info active')
 		->add_title_search_button([['server_status','in',['pending','pending-setup','pend-approval']]], _('Pending'), 'info')
