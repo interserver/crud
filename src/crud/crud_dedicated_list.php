1c09472e96aa2196abf600b5c179a81ef61f9b6e
Ganesh R <kumar.interserver@gmail.com>
2023-10-25 18:18:02 +0530

    IP field display on servers list page

--- src/crud/crud_dedicated_list.php
+++ src/crud/crud_dedicated_list.php
@@ -14,12 +14,12 @@ use \MyCrud\Crud;
  */
 function crud_dedicated_list()
 {
-	Crud::init('select server_id, account_lid, server_hostname, server_status from servers left join accounts on account_id=server_custid', 'servers')
+	Crud::init('select server_id, account_lid, primary_ipv4, server_hostname, server_status from servers left join accounts on account_id=server_custid left join assets on order_id=server_id', 'servers')
         ->set_limit_custid_role('list_all')
 		->set_title(_('Dedicated List'))
 		->set_order('server_id', 'desc')
 		->enable_labels()
-		->set_labels(['server_id' => _('ID'),'account_lid' => _('Client'),'server_hostname' =>  _('Server Name'), 'server_status' => _('Status')])
+		->set_labels(['server_id' => _('ID'),'account_lid' => _('Client'), 'primary_ipv4' => 'IP', 'server_hostname' =>  _('Server Name'), 'server_status' => _('Status')])
 		->add_header_button($GLOBALS['tf']->link('index.php', 'choice=none.order_server'), _('Order'), 'primary', 'shopping-cart', _('Order Server'), 'client')
 		->add_title_search_button([['server_status','in',['active','active-billing']]], _('Active'), 'info active')
 		->add_title_search_button([['server_status','in',['pending','pending-setup','pend-approval']]], _('Pending'), 'info')
