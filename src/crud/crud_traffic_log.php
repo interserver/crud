d4477cfe61795abc60391f6b16e00e8da526c447
Joe Huss <detain@interserver.net>
2025-04-22 09:28:21 -0400

    minor update

--- src/crud/crud_traffic_log.php
+++ src/crud/crud_traffic_log.php
@@ -23,7 +23,7 @@ function crud_traffic_log()
     // IF(accounts_ext.account_id is null,'<i class=\"fa fa-remove\">',concat('<a href=\"index.php?choice=none.edit_customer&customer=',accounts_ext.account_id,'\"><i class=\"fa fa-search\"></i></a>')) AS affiliate
     // LEFT JOIN accounts_ext ON account_key = 'referrer_coupon' AND account_value = name
     Crud::init("select id,timestamp,method,status,custid,sessionid,client_ip,uri from traffic_log")
-        ->set_title(_('Coupons'))
+        ->set_title(_('Traffic Log'))
         ->enable_labels()
         ->set_order('id', 'desc')
         ->disable_delete()
