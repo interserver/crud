c46cd743950d3b5523a89a500984b3a90db57741
Ganesh R <kumar.interserver@gmail.com>
2025-05-07 19:55:16 +0530

    check payment filter added

--- src/crud/crud_invoices.php
+++ src/crud/crud_invoices.php
@@ -14,8 +14,20 @@ use \MyCrud\Crud;
  */
 function crud_invoices()
 {
-    Crud::init('select * from invoices')
-        ->set_limit_custid_role('list_all')
-        ->set_title(_('Invoices'))
-        ->go();
+    if ($GLOBALS['tf']->variables->request['type'] == 'check') {
+        Crud::init("SELECT ".
+            "date_format(invoices_date, '%Y-%m-%d') as date, concat(invoices_currency,' ',invoices_amount) AS invoice_amount, account_lid as customer, invoices_description as description, invoices_module AS module, invoices_service as service ".
+            "FROM invoices ".
+            "LEFT JOIN accounts ON account_id = invoices_custid ".
+            "WHERE invoices_type = 17 AND invoices_date LIKE '2025%'")
+            ->set_limit_custid_role('list_all')
+            ->enable_fluid_container()
+            ->set_title(_('Invoices'))
+            ->go();
+    } else {
+        Crud::init('select * from invoices')
+            ->set_limit_custid_role('list_all')
+            ->set_title(_('Invoices'))
+            ->go();
+    }
 }
